b84291621a6ec2186ce54e7e3423dd6bdfd617d8
Daniel <crtzdanielg@gmail.com>
2025-07-28 02:02:18 +0800

    Initial commit 2

--- js/script-onepage.js
+++ js/script-onepage.js
@@ -21,7 +21,7 @@ if (geoBg) {
     angle += 0.2;
     geoBg.style.background = `repeating-linear-gradient(${135 + angle}deg, transparent, transparent 40px, #c8956d 41px, transparent 42px), repeating-linear-gradient(${-135 - angle}deg, transparent, transparent 40px, #d4a574 41px, transparent 42px)`;
   }, 60);
-}
+} 
 
 window.addEventListener('scroll', function() {
   const topbar = document.querySelector('.topbar');
